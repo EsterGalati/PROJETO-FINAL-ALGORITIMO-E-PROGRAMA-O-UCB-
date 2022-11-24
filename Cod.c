ccc7f436a4212a751a9201f85e27ae3a78a30468
LuizaMunis <luizamunisnascimento@gmail.com>
2022-11-23 22:53:41 -0300

    sobe

--- Cod.c
+++ Cod.c
@@ -3,52 +3,93 @@
 #include <strings.h> //biblioteca de strings
 int main (){
 
-    int sessoes, idade1, assistiram1, sexo1;
-    int idade2, assistiram2, sexo2; //variaveis que serao usadas no codigo
-    char filme1 [20], sexo1, ingressos1; //O sexo deve ser dado em f ou m e se o ingresso foi meia ou inteira em m ou i
-    char filme2 [20], sexo2, ingressos2; 
+    int sessoes, idade1, assistiram1, contador;
+ //variaveis que serao usadas no codigo
+    char filme1[20];
+    char sexo1, ingressos1; //O sexo deve ser dado em f ou m e se o ingresso foi meia ou inteira em m ou i
+
+   int  idade2, assistiram2;;
+ //variaveis que serao usadas no codigo
+    char filme2[20];
+    char sexo2, ingressos2;
+
     printf("Nome: Luiza Munis Nascimento\nMatricula: UC22201024\nCurso:Engenharia de software.");
     printf("\n\nNome: Ester Luiza Siqueira Correa Galati\nMatricula: UC22200640\nCurso:Ciencia da Computacao ");
     printf("\n\nNome: Cristiano Mundin Borges Junior \nMatricula: UC22200034\nCurso:Ciencia da Computacao ");
 
 printf("\n\nQuantidades de sessoes:");
 scanf("%d", &sessoes);// Pergunta quantas sessões tem ao usuário.
-while (sessoes >2 || sessoes <1){
+while (sessoes >2 || sessoes <1){ // corrigir
     printf("Numero Invalido!");
 
 printf("\n\nQuantidades de sessoes:");
 scanf("%d", &sessoes);
 }
-
+fflush (stdin);
 /////////////////////////////////////////////////////////////////////
 
 printf("Nome do filme:"); //dividir as informacoes de cada sessao
-scanf("%s", &filme1);
+fgets(filme1, 20,stdin);
 fflush (stdin);
 
-printf("Quantas pessoas assistiram ao primeiro filme?");
+
+printf("\nQuantas pessoas assistiram ao primeiro filme?");
 scanf("%d", &assistiram1);
 
-printf("Digite o sexo:");
-scanf("%c%d", &sexo1);
+while (assistiram1<=10){ 
+    printf("Numero Invalido!");
+
+printf("\nQuantas pessoas assistiram ao primeiro filme?");
+scanf("%d", &assistiram1);
+}
+
+for(contador = 0; contador < assistiram1; contador++){//corrigir contador
+    fflush (stdin);
+
+printf("Digite o sexo da %d° pessoa:", contador +1);// corrigir para aceitar só f e m
+scanf("%c", &sexo1);
 fflush (stdin);
 
 printf("Digite a idade");
-scanf("%c%d", &idade1);
+scanf("%d", &idade1);
+fflush (stdin);
 
 printf("Digite o tipo de ingresso, inteira ou meia:");
-scanf("%c", &ingressos1);
+scanf("%c", &ingressos1); // corrigir para aceitar só i e m
 fflush (stdin);
-
-
-///////////////////////////////////////////////////////////////////// MEXI AQ
+}
+///////////////////////////////////////////////////////////////////////////
 
 printf("Nome do filme:"); //dividir as informacoes de cada sessao
-scanf("%s", &filme2);
+fgets(filme2, 20,stdin);
 fflush (stdin);
 
-printf("Quantas pessoas assistiram ao primeiro filme?");
+
+printf("\nQuantas pessoas assistiram ao primeiro filme?");
 scanf("%d", &assistiram2);
+
+while (assistiram1<=10){ 
+    printf("Numero Invalido!");
+
+printf("\nQuantas pessoas assistiram ao primeiro filme?");
+scanf("%d", &assistiram2);
+}
+
+for(contador = 0; contador < assistiram1; contador++){//corrigir contador
+    fflush (stdin);
+
+printf("Digite o sexo da %d° pessoa:", contador +1);// corrigir para aceitar só f e m
+scanf("%c", &sexo2);
+fflush (stdin);
+
+printf("Digite a idade");
+scanf("%d", &idade2);
+fflush (stdin);
+
+printf("Digite o tipo de ingresso, inteira ou meia:");
+scanf("%c", &ingressos2); // corrigir para aceitar só i e m
+fflush (stdin);
+}
 
 
 }
